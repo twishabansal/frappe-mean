cc08c343caff2d04e2aaaec0d6dcbaa61f4aaf9d
Twisha Bansal <twishabansal07@gmail.com>
2022-01-24 22:27:11 +0530

    Change member schema

--- models/members.js
+++ models/members.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
+const bookIssuedSchema = new mongoose.Schema({
+    bookID: String,
+    rentOwed: Number
+});
+
 const MembersSchema = new mongoose.Schema({
     memberID: String,
     name: String,
     contactNo : String,
     dateOfJoining : Date,
-    currentBooksIssued: [String],
-    outstandingDebt: Number,
-    totalPaidTillDate: Number
+    currentBooksIssued: [bookIssuedSchema],
+    outstandingDebt: {type: Number, default: 0},
+    totalPaidTillDate: {type: Number, default: 0}
 }, { versionKey: false });
 
 let Members = mongoose.model('Member', MembersSchema);
