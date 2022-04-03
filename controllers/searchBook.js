f0cd33a407adfb38d005eb29e7b84e16899bc32a
Twisha Bansal <twishabansal07@gmail.com>
2022-04-03 14:35:05 +0530

    Cleaned code for search book

--- controllers/searchBook.js
+++ controllers/searchBook.js
@@ -1,24 +1,21 @@
 const Books = require("../models/books");
 
 function searchBookByTitle(req, res) {
-    Books.find({"title": {$regex : req.params.title}},
-    (err, book) => {
-        if (err) {
-            throw err;
-        } else {
-            res.send(book);
-        }
-    })
+    Books.find({"title": {$regex : req.params.title}})
+    .then((book) => {
+        res.send(book);
+    }).catch((err) => {
+        throw err;
+    });
 };
 
 function searchBookByAuthor(req, res) {
-    Books.find({"authors": {$regex : req.params.author}}, (err, book) => {
-        if (err) {
-            throw err;
-        } else {
-            res.send(book);
-        }
-    })
+    Books.find({"authors": {$regex : req.params.author}})
+    .then((book) => {
+        res.send(book);
+    }).catch((err) => {
+        throw err;
+    });
 };
 
 module.exports = {searchBookByAuthor, searchBookByTitle}
