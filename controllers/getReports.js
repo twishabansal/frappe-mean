9abf9c53a760724cba33bfe0e4c2fcc48ccf49af
Twisha Bansal <twishabansal07@gmail.com>
2022-04-04 10:13:31 +0530

    Used promises for getting reports

--- controllers/getReports.js
+++ controllers/getReports.js
@@ -1,18 +1,16 @@
 const Members = require("../models/members");
 const Books = require("../models/books");
 
-async function getReport(req, res) {
-    try {
-        let highestPayingMembers = await Members.find().sort({"totalPaidTillDate" : "desc"}).limit(5);
-        try {
-            let highestRentedBooks =  await Books.find().sort({"numTimesRented" : "desc"}).limit(5);
+function getReport(req, res) {
+    Members.find().sort({"totalPaidTillDate" : "desc"}).limit(5).then((highestPayingMembers) => {
+        Books.find().sort({"numTimesRented" : "desc"}).limit(5).then((highestRentedBooks) => {
             res.send({"highestPayingMembers" : highestPayingMembers, "highestRentedBooks": highestRentedBooks});
-        } catch (err) {
+        }).catch((err) => {
             throw err;
-        }
-    } catch (err) {
+        })
+    }).catch((err) => {
         throw err;
-    }
+    });
 }
  
 module.exports = {getReport};
